81e36117cf83a3a24fb391649598bdcfbef2e5c2
Bintang Pancahaya <bintang.panca007@gmail.com>
2026-03-08 19:21:39 +0700

    Praktikum 2.1

--- database/migrations/2026_03_08_121508_create_m_supplier_table.php
+++ database/migrations/2026_03_08_121508_create_m_supplier_table.php
@@ -10,12 +10,15 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('m_supplier', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('m_supplier', function (Blueprint $table) {
+        $table->id('supplier_id');
+        $table->string('supplier_kode', 10)->unique();
+        $table->string('supplier_nama', 100);
+        $table->string('supplier_alamat', 255);
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
