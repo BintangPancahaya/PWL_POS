ef01cb98b80663424a7418cc418634354fbf34d6
Bintang Pancahaya <bintang.panca007@gmail.com>
2026-03-16 20:40:14 +0700

    Praktikum 2.4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,10 +7,18 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
+            <th>ID</th>
+            <th>Level ID</th>
+            <th>Username</th>
+            <th>Nama</th>
         </tr>
         <tr>
-            <td>{{ $data }}</td>
+            <tr>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->level_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+            </tr>
         </tr>
     </table>
 </body>
